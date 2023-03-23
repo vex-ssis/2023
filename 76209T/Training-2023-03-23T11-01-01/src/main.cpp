6b2a6cc35a36ec5302c7a4ca0ac364f325e62e40
SSIS students <43933271+kreier@users.noreply.github.com>
2023-03-23 18:01:53 +0700

    first cpp for VEX

---
+++ 76209T/Training-2023-03-23T11-01-01/src/main.cpp
@@ -0,0 +1,48 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       mkreier                                                   */
+/*    Created:      Thu Mar 23 2023                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Motor1               motor         1               
+// Controller1          controller                    
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+double fb;
+int counter;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  Motor1.setVelocity(40,percent);
+  fb = Controller1.Axis3.position(percent);
+  Motor1.spin(forward);
+  Controller1.Screen.clearScreen();
+  while(true) {
+    fb = Controller1.Axis3.position(percent);
+    if(fb < 0) {
+      Motor1.spin(reverse);
+    }
+    else {
+      Motor1.spin(reverse);
+    }
+    Motor1.setVelocity(fb, percent);
+    Controller1.Screen.setCursor(0,0);
+    Controller1.Screen.print("Input value:");
+    Controller1.Screen.print(fb);
+    Controller1.Screen.print("  ");
+    Controller1.Screen.newLine();
+    Controller1.Screen.print("Counter: ");
+    Controller1.Screen.print(counter);
+    counter++;
+  }
+}
