40a83da11d4bd735095b3b7fc8ab5b8cc6d581bf
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-04-06 19:45:48 +0700

    standard project in cpp

---
+++ 76209T/cpp/Cpp-2023-04-06T12-44-57/src/main.cpp
@@ -0,0 +1,26 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       mk                                                        */
+/*    Created:      Thu Apr 06 2023                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Drivetrain           drivetrain    12, 19, 6       
+// Controller1          controller                    
+// GPS13                gps           13              
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
---
+++ 76209T/cpp/Cpp-2023-04-06T12-44-57/src/robot-config.cpp
@@ -0,0 +1,90 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor LeftDriveSmart = motor(PORT12, ratio18_1, false);
+motor RightDriveSmart = motor(PORT19, ratio18_1, true);
+inertial DrivetrainInertial = inertial(PORT6);
+smartdrive Drivetrain = smartdrive(LeftDriveSmart, RightDriveSmart, DrivetrainInertial, 299.24, 320, 40, mm, 1);
+controller Controller1 = controller(primary);
+gps GPS13 = gps(PORT13, 0.00, 0.00, mm, 180);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+// define variables used for controlling motors based on controller inputs
+bool DrivetrainNeedsToBeStopped_Controller1 = true;
+
+// define a task that will handle monitoring inputs from Controller1
+int rc_auto_loop_function_Controller1() {
+  // process the controller input every 20 milliseconds
+  // update the motors based on the input values
+  while(true) {
+    if(RemoteControlCodeEnabled) {
+      // calculate the drivetrain motor velocities from the controller joystick axies
+      // left = Axis3 + Axis4
+      // right = Axis3 - Axis4
+      int drivetrainLeftSideSpeed = Controller1.Axis3.position() + Controller1.Axis4.position();
+      int drivetrainRightSideSpeed = Controller1.Axis3.position() - Controller1.Axis4.position();
+      
+      // check if the values are inside of the deadband range
+      if (abs(drivetrainLeftSideSpeed) < 5 && abs(drivetrainRightSideSpeed) < 5) {
+        // check if the motors have already been stopped
+        if (DrivetrainNeedsToBeStopped_Controller1) {
+          // stop the drive motors
+          LeftDriveSmart.stop();
+          RightDriveSmart.stop();
+          // tell the code that the motors have been stopped
+          DrivetrainNeedsToBeStopped_Controller1 = false;
+        }
+      } else {
+        // reset the toggle so that the deadband code knows to stop the motors next time the input is in the deadband range
+        DrivetrainNeedsToBeStopped_Controller1 = true;
+      }
+      
+      // only tell the left drive motor to spin if the values are not in the deadband range
+      if (DrivetrainNeedsToBeStopped_Controller1) {
+        LeftDriveSmart.setVelocity(drivetrainLeftSideSpeed, percent);
+        LeftDriveSmart.spin(forward);
+      }
+      // only tell the right drive motor to spin if the values are not in the deadband range
+      if (DrivetrainNeedsToBeStopped_Controller1) {
+        RightDriveSmart.setVelocity(drivetrainRightSideSpeed, percent);
+        RightDriveSmart.spin(forward);
+      }
+    }
+    // wait before repeating the process
+    wait(20, msec);
+  }
+  return 0;
+}
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  Brain.Screen.print("Device initialization...");
+  Brain.Screen.setCursor(2, 1);
+  // calibrate the drivetrain Inertial
+  wait(200, msec);
+  DrivetrainInertial.calibrate();
+  Brain.Screen.print("Calibrating Inertial for Drivetrain");
+  // wait for the Inertial calibration process to finish
+  while (DrivetrainInertial.isCalibrating()) {
+    wait(25, msec);
+  }
+  // reset the screen now that the calibration is complete
+  Brain.Screen.clearScreen();
+  Brain.Screen.setCursor(1,1);
+  task rc_auto_loop_task_Controller1(rc_auto_loop_function_Controller1);
+  wait(50, msec);
+  Brain.Screen.clearScreen();
+}
