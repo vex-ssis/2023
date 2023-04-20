e7c3933320d133c4def3986fa6533ad27fb7f206
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-04-20 20:31:03 +0700

    drive mechanum with cpp

---
+++ 76209G/CG-2023-04-20/src/robot-config.cpp
@@ -0,0 +1,28 @@
+#include "vex.h"
+
+using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
+
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+motor FL = motor(PORT1, ratio18_1, false);
+motor BL = motor(PORT11, ratio18_1, false);
+motor FR = motor(PORT10, ratio18_1, false);
+motor BR = motor(PORT20, ratio18_1, false);
+controller Controller1 = controller(primary);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void vexcodeInit( void ) {
+  // nothing to initialize
+}
