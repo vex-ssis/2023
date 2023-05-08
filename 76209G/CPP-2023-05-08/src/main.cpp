85f2ae7e436b97d8905ae396bf163ad8e43a353b
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-05-08 21:25:43 +0700

    fixed code to match python

--- 76209G/CPP-2023-05-08/src/main.cpp
+++ 76209G/CPP-2023-05-08/src/main.cpp
@@ -30,14 +30,55 @@ int main() {
   Controller1.Screen.setCursor(1, 1);
   Controller1.Screen.print("User control   ");
   Controller1.Screen.newLine();
-  float mFL = (  1,  1, -1);
-  float mBL = ( -1,  1,  1);
-  float mFR = ( -1,  1, -1);
-  float mBR = (  1,  1,  1) ;  
+  float mFL[] = {  1,  1, -1};
+  float mBL[] = { -1,  1,  1};
+  float mFR[] = { -1,  1, -1};
+  float mBR[] = {  1,  1,  1};  
   while (true) {
     float fb = Controller1.Axis3.position(percent);
     float lr = Controller1.Axis1.position(percent);
     float jaw = Controller1.Axis4.position(percent);
-
+    float speed_FL = mFL[0]*fb + mFL[1]*lr + mFL[2]*jaw;
+    float speed_BL = mBL[0]*fb + mBL[1]*lr + mBL[2]*jaw;
+    float speed_FR = mFR[0]*fb + mFR[1]*lr + mFR[2]*jaw;
+    float speed_BR = mBR[0]*fb + mBR[1]*lr + mBR[2]*jaw;
+    MotorFL.spin(forward, speed_FL, volt);
+    MotorBL.spin(forward, speed_BL, volt);
+    MotorFR.spin(forward, speed_FR, volt);
+    MotorBR.spin(forward, speed_BR, volt);
+    Controller1.Screen.setCursor(2, 1);
+    Controller1.Screen.print(fb);
+    Controller1.Screen.print(lr);
+    Controller1.Screen.print(jaw);
+    Controller1.Screen.print("    ");
+    Controller1.Screen.newLine();
+    Controller1.Screen.print(speed_FL);
+    Controller1.Screen.print(speed_BL);
+    Controller1.Screen.print(speed_FR);
+    Controller1.Screen.print(speed_BR);
+    Controller1.Screen.print("      ");
+    if (Controller1.ButtonR1.pressing()) 
+    {
+      MotorRoller.spin(forward);
+    }
+    else if (Controller1.ButtonR2.pressing())
+    {
+      MotorRoller.spin(reverse);
+    }
+    else {
+      MotorRoller.stop();
+    }
+    if (Controller1.ButtonL1.pressing())
+    {
+      MotorIntake.spin(forward);
+    }
+    else if (Controller1.ButtonL2.pressing())
+    {
+      MotorIntake.spin(reverse);
+    }
+    else
+    {
+        MotorIntake.stop();
+    }
   }
 }
