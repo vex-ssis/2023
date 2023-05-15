dc611210de661b7e5687139e39e17396cb885d00
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-05-15 18:24:45 +0700

    reduced acceleration and turning speed

--- 76209S/SPEED3/src/main.cpp
+++ 76209S/SPEED3/src/main.cpp
@@ -8,6 +8,11 @@
 /*----------------------------------------------------------------------------*/
 
 // ---- START VEXCODE CONFIGURED DEVICES ----
+// Robot Configuration:
+// [Name]               [Type]        [Port(s)]
+// Controller1          controller                    
+// MotorL               motor         1               
+// MotorR               motor         10              
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -17,5 +22,21 @@ using namespace vex;
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  
+
+  Brain.Screen.print("User control.");
+  Controller1.Screen.clearScreen();
+  Controller1.Screen.print("User control");
+  float speed = 0;
+  float jaw = 0;
+  float speedL = 0;
+  float speedR = 0;
+  float k = 0.00001;
+  while (true) {
+    speed = Controller1.Axis3.position(percent);
+    jaw = Controller1.Axis1.position(percent);
+    speedL += (( 0.2 * speed - 0.05 * jaw) - speedL) * k;
+    speedR += ((-0.2 * speed - 0.05 * jaw) - speedR) * k;
+    MotorL.spin(forward,speedL,volt);
+    MotorR.spin(forward,speedR,volt);
+  }
 }
--- 76209S/SPEED3/src/robot-config.cpp
+++ 76209S/SPEED3/src/robot-config.cpp
@@ -1,15 +1,26 @@
 #include "vex.h"
 
 using namespace vex;
+using signature = vision::signature;
+using code = vision::code;
 
-// A global instance of brain used for printing to the V5 brain screen
-brain Brain;
+// A global instance of brain used for printing to the V5 Brain screen
+brain  Brain;
+
+// VEXcode device constructors
+controller Controller1 = controller(primary);
+motor MotorL = motor(PORT1, ratio18_1, false);
+motor MotorR = motor(PORT10, ratio18_1, false);
+
+// VEXcode generated functions
+// define variable for remote controller enable/disable
+bool RemoteControlCodeEnabled = true;
 
 /**
  * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
- *
+ * 
  * This should be called at the start of your int main function.
  */
-void vexcodeInit(void) {
-  // Nothing to initialize
+void vexcodeInit( void ) {
+  // nothing to initialize
 }
