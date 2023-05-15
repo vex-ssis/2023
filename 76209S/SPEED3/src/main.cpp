7b2d22bc8c57a08e024ea1fe2b55ffdc7b92542c
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-05-15 17:47:56 +0700

    organize more granulated control

---
+++ 76209S/SPEED3/src/main.cpp
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/*                                                                            */
+/*    Module:       main.cpp                                                  */
+/*    Author:       mk                                                        */
+/*    Created:      Mon May 15 2023                                           */
+/*    Description:  V5 project                                                */
+/*                                                                            */
+/*----------------------------------------------------------------------------*/
+
+// ---- START VEXCODE CONFIGURED DEVICES ----
+// ---- END VEXCODE CONFIGURED DEVICES ----
+
+#include "vex.h"
+
+using namespace vex;
+
+int main() {
+  // Initializing Robot Configuration. DO NOT REMOVE!
+  vexcodeInit();
+  
+}
