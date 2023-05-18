0f2fbf31d911045a3d9cf220912ffec98473e86d
Matthias Kreier <43933271+kreier@users.noreply.github.com>
2023-05-18 17:19:43 +0700

    Highscore 39 points autonomous

---
+++ 76209T/T5/include/robot-config.h
@@ -0,0 +1,15 @@
+using namespace vex;
+
+extern brain Brain;
+
+// VEXcode devices
+extern controller Controller1;
+extern smartdrive Drivetrain;
+extern motor Roller;
+
+/**
+ * Used to initialize code/tasks/devices added using tools in VEXcode Pro.
+ * 
+ * This should be called at the start of your int main function.
+ */
+void  vexcodeInit( void );
